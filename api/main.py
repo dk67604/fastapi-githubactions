1decf925251d2da368bb49b17220cff78f1ec7bd
dk67604 <dharkumar@microsoft.com>
2025-02-17 18:03:19 -0800

    OIDC To AWS Accoun

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from mangum import Mangum # Wrapper for AWS Lambda to run FastAPI
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+handler = Mangum(app)
